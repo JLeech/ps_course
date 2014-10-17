675c1e8fc59329e8d1e717c5db155feb55d1d50f
Eve <eveleech@gmail.com>
2014-10-17 05:10:02 +0400

    test line added

--- lexer.rb
+++ lexer.rb
@@ -1,5 +1,17 @@
 require 'singleton'
 
+class Command
+
+	attr_accessor :name
+	attr_accessor :flags
+	attr_accessor :arguments
+
+	def initialize(token)
+
+	end
+
+end
+
 class Position
 	attr_accessor :line
 	attr_accessor :position
@@ -34,6 +46,30 @@ class Message
 	end
 end
 
+class FlagsTable
+	include Singleton
+
+	attr_accessor :flags
+	attr_accessor :file
+
+	def initialize
+		@file = "flags.txt"
+		@flags = {}
+		lines = IO.readlines(@file)
+		lines.each { |line| line.chomp! }
+		lines.each do |line|
+			words = line.split(" ")
+			@flags[words.first] = words.drop(1)
+		end
+	end
+
+	def has_flag?(command,flag)
+		return true if @flags[command].include?(flag)
+		return false
+	end
+
+end
+
 class CommandTable
 	include Singleton
 
@@ -44,30 +80,36 @@ class CommandTable
 		@file = "commands.txt"
 		@commands = IO.readlines(@file)
 		@commands.each { |command| command.chomp! }
+
+	end
+
+	def command?(token)
+		if @commands.include?(token)
+			return true
+		else
+			return false
+		end
 	end
 end
 
-module LexerState
-	
-	COMMAND = 0
-	FLAGS = 1
-	ARGUMENTS = 2
-	PIPE = 3
-
-end
-
 class Lexer
-	include LexerState
 	attr_accessor :pre_tokens
 	attr_accessor :state
 	attr_accessor :line
 	attr_accessor :messages
+
+	COMMAND = 0
+	ARGS = 1
 
 	def initialize(line)
 		@pre_tokens = line.split(" ")
 		@state = COMMAND
 		@tokens = []
 		@messages =[]
+	end
+
+	def parse_tokens
+
 	end
 
 	def get_token
@@ -78,6 +120,9 @@ class Lexer
 
 end
 
-ct = CommandTable.instance
-lex = Lexer.new("Make-Object -directory dir-dir | Rename-Object -directory new_dir | Make-Object  ./$_/inside.txt | Zip-Object -recursive dir-dir")
-lex.get_token
+ft = FlagsTable.instance
+lex = Lexer.new("Make-Object -directory dir-dir | Rename-Object -directory new_dir | Make-Object ./$_/inside.txt | Zip-Object -recursive dir-dir")
+puts ft.has_flag?("Make-Object","-directory")
+
+
+
